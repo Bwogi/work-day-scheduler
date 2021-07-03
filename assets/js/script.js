8e05d177a8fa9091909ff895a85132cf8d40e34f
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-07-03 14:27:50 -0700

    making time block dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,52 @@
-// current day 
-var now = moment().format('MMMM Do YYYY, h:mm:ss a');
-// console.log(today)
-var currentDayEl = document.getElementById('currentDay');
-// console.log(currentDayEl)
-currentDayEl.textContent = now;
+// const { parseTwoDigitYear } = require("moment");
+
+var saveBtnEl = document.querySelector('.saveBtn');
+var currentDayEl = document.querySelector("#currentDay");
+
+// DOM header day date addition
+var timeNow = moment().format('LLLL');
+console.log(timeNow)
+$("#currentDay").text(timeNow);
+
+// what is the time now(hourly)?
+var checkTime = moment().hour();
+console.log(checkTime);
+
+// indicate current time on calendar 
+$(".time-block").each(function() {
+    var hourBlock = $(this).attr("id");
+    if (hourBlock == checkTime) {
+        $(this).addClass("present");
+        $(this).remove("past");
+        $(this).remove("future");
+    } else if (hourBlock < checkTime) {
+        $(this).addClass("past");
+        $(this).remove("future");
+        $(this).remove("present");
+    } else if (hourBlock > checkTime) {
+        $(this).addClass("future");
+        $(this).remove("past");
+        $(this).remove("present");
+    }
+
+})
+
+// Save the day's date? 
+var checkDate = moment().format('L');
+console.log(checkDate);
+localStorage.setItem("savedDate", checkDate);
+
+
+
+// check if calendar has changed from one day to another 
+var dateInStorage = localStorage.getItem("savedDate");
+
+if (checkDate != dateInStorage) {
+    localStorage.clear();
+}
+
+
+
+
+
+// when button is clicked
